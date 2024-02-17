1dd86a8d2f35acd6509e82818e2e0def4a082c23
Rashaad0554 <112050207+Rashaad0554@users.noreply.github.com>
2024-02-17 17:28:39 -0500

    Update Clock.ino

--- Clock.ino
+++ Clock.ino
@@ -4,6 +4,7 @@
 #define BUTTON_MODE 21
 #define BUTTON_INC 19
 #define BUTTON_DEC 18
+#define BUZZER 5
 
 bool bellState = false;
 //Structure example to receive data
@@ -13,18 +14,23 @@ typedef struct test_struct {
 } test_struct;
 
 //Create a struct_message called myData
-test_struct rashaad;
+test_struct reciever;
 
 //callback function that will be executed when data is received
 void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
-  memcpy(&rashaad, incomingData, sizeof(rashaad));
+  memcpy(&reciever, incomingData, sizeof(reciever));
   // Serial.print("Bytes received: ");
   // Serial.println(len);
-  if (rashaad.bellPressed == 1 && bellState == false) {
+  if (reciever.bellPressed == 1 && bellState == false) {
     bellState = true;
     Serial.print("\nOPEN NA NOOR!!!");
+    tone(BUZZER, 660);
+    delay(250);
+    tone(BUZZER, 552);
+    delay(250);
+    noTone(BUZZER);
   }
-  if (rashaad.bellPressed == 0 && bellState == true) {
+  if (reciever.bellPressed == 0 && bellState == true) {
     bellState = false;
   }
 }
@@ -34,7 +40,7 @@ int mode = 0;  //Max 3
 bool modeState = false;
 
 int incPressed;
-int hours = 0;  //Max 23
+int hour = 0;  //Max 23
 bool incState = false;
 
 int decPressed;
@@ -43,7 +49,8 @@ bool decState = false;
 
 void setup() {
   //Initialize Serial Monitor
-  Serial.begin(115200);
+  Serial.begin(115200);  
+  Serial.printf("\nmode = %d, %02d:%02d", mode, hour, minute);
   //Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
   //Init ESP-NOW
@@ -58,7 +65,6 @@ void setup() {
   pinMode(BUTTON_MODE, INPUT_PULLDOWN);
   pinMode(BUTTON_INC, INPUT_PULLDOWN);
   pinMode(BUTTON_DEC, INPUT_PULLDOWN);
-  Serial.printf("\nmode = %d, %02d:%02d", mode, hours, minute);
 }
 
 void loop() {
@@ -66,13 +72,28 @@ void loop() {
   button(&modePressed, &modeState, &mode, 3, 1);
 
   switch (mode) {
+    case 0:
+      delay(60000);
+      if ((minute + 1 > 59) {
+        if ((hour + 1) > 23) {
+          minute = 0;
+          hour = 0;
+        }
+        else {
+          minute = 0;
+          hour++;
+        }
+      }
+      else {
+        minute++;
+      }
     case 1: break;  //ENTIRE CLOCK IS BLINKING
     case 2:         //HOUR IS BLINKING
       incPressed = digitalRead(BUTTON_INC);
-      button(&incPressed, &incState, &hours, 23, 1);
+      button(&incPressed, &incState, &hour, 23, 1);
 
       decPressed = digitalRead(BUTTON_DEC);
-      button(&decPressed, &decState, &hours, 23, -1);
+      button(&decPressed, &decState, &hour, 23, -1);
 
       break;
     case 3:  //MINUTES IS BLINKING
@@ -98,6 +119,6 @@ void button(int *buttonPressed, bool *buttonState, int *curr, int max, int incre
   }
   if (*buttonPressed == 0 && *buttonState == true) {  // if button isn't pressed
     *buttonState = false;
-    Serial.printf("\nmode = %d, %02d:%02d", mode, hours, minute);
+    Serial.printf("\nmode = %d, %02d:%02d", mode, hour, minute);
   }
 }
