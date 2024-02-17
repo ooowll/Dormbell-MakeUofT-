905a9e0fce41d96b4e64f1d12fe0dc051bb98831
Rashaad0554 <112050207+Rashaad0554@users.noreply.github.com>
2024-02-17 11:41:33 -0500

    Update Clock.ino

--- Clock.ino
+++ Clock.ino
@@ -1,9 +1,36 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
 #define BUTTON_MODE 21
 #define BUTTON_INC 19
 #define BUTTON_DEC 18
 
+bool bellState = false;
+//Structure example to receive data
+//Must match the sender structure
+typedef struct test_struct {
+  int bellPressed;
+} test_struct;
+
+//Create a struct_message called myData
+test_struct rashaad;
+
+//callback function that will be executed when data is received
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
+  memcpy(&rashaad, incomingData, sizeof(rashaad));
+  // Serial.print("Bytes received: ");
+  // Serial.println(len);
+  if (rashaad.bellPressed == 1 && bellState == false) {
+    bellState = true;
+    Serial.print("\nOPEN NA NOOR!!!");
+  }
+  if (rashaad.bellPressed == 0 && bellState == true) {
+    bellState = false;
+  }
+}
+
 int modePressed;
-int mode = 0; //Max 3
+int mode = 0;  //Max 3
 bool modeState = false;
 
 int incPressed;
@@ -15,7 +42,19 @@ int minute = 0;  //Max 59
 bool decState = false;
 
 void setup() {
+  //Initialize Serial Monitor
   Serial.begin(115200);
+  //Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  //Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+  // clock button setup
   pinMode(BUTTON_MODE, INPUT_PULLDOWN);
   pinMode(BUTTON_INC, INPUT_PULLDOWN);
   pinMode(BUTTON_DEC, INPUT_PULLDOWN);
@@ -28,7 +67,7 @@ void loop() {
 
   switch (mode) {
     case 1: break;  //ENTIRE CLOCK IS BLINKING
-    case 2: //HOUR IS BLINKING
+    case 2:         //HOUR IS BLINKING
       incPressed = digitalRead(BUTTON_INC);
       button(&incPressed, &incState, &hours, 23, 1);
 
@@ -36,7 +75,7 @@ void loop() {
       button(&decPressed, &decState, &hours, 23, -1);
 
       break;
-    case 3: //MINUTES IS BLINKING
+    case 3:  //MINUTES IS BLINKING
       incPressed = digitalRead(BUTTON_INC);
       button(&incPressed, &incState, &minute, 59, 1);
 
@@ -61,5 +100,4 @@ void button(int *buttonPressed, bool *buttonState, int *curr, int max, int incre
     *buttonState = false;
     Serial.printf("\nmode = %d, %02d:%02d", mode, hours, minute);
   }
-  
 }
